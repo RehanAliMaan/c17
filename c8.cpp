a304200bb091939233baa0a0c7241fcaa11bd8e1
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-10-19 19:25:26 +0500

    Add files via upload

---
+++ c8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int p,r,t,i;
+	i=p*r*t/100;
+	cout<<"enter principal amount,rate,time";
+	cin>>p>>r>>t;
+	cout<<"/n your principal amount is=Rs"<<p<<endl;
+	cout<<"/n your rate is=%"<<r<<endl;
+	cout<<"/n your time is=yrs"<<t<<endl;
+	cout<<"/n your interest is=Rs"<<i<<endl;
+}
+	
+
+	
+	
+	
